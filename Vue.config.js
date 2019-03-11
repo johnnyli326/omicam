7d9a7cc9a386ccda967380fe7da4069ff6bffd0f
Johnny <LiHsiaoChiang0326@gmail.com>
2019-03-11 18:23:24 +0800

    piblic path

---
+++ Vue.config.js
@@ -0,0 +1,7 @@
+/* eslint no-param-reassign: "error" */
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const path = require('path');
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+module.exports = {
+  publicPath: './',
+};
